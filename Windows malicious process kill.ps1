c90ce2e7d9dadef3bff69503fe7829b1d16b3534
bhawk <bhawk>
2018-02-26 00:05:53 +0530

    windows powershelll firewall and process automation
    Kill process and add firewall rule based and on given hash and ip address

---
+++ Windows malicious process kill.ps1
@@ -0,0 +1,24 @@
+﻿$file="C:\Users\testman\Desktop\testrun\BlockList.txt"
+$emotethash=Import-Csv $file
+$processpath=(Get-Process).Path | select -Unique
+
+$processhash=$processpath| % {
+Get-FileHash -Path $_
+}
+
+
+ForEach($ehash in $emotethash)
+{
+	ForEach($phash in $processhash)
+	{
+
+$phash.Hash
+$phash.Path
+
+		If($phash.Hash -eq $ehash)
+		{
+			kill -Name $(Split-Path $phash.Path -Leaf) -Force
+            Remove-Item -Path $phash.Path -ErrorAction SilentlyContinue
+		}
+	}
+}
---
+++ windows firewall block ip from file.ps1
@@ -0,0 +1,11 @@
+$ProfileType = "any"
+$ranges = get-content $file
+$linecount = $ranges.count
+$textranges = [System.String]::Join(",",$($ranges[0..$($linecount - 1)]))
+netsh.exe advfirewall firewall add rule name="Emotet_block" dir=in action=block localip=any remoteip="$textranges" description="$description" profile="$profiletype" interfacetype="$interfacetype"
+#if (-not $?) { "`nFailed to create '$rulename-#$icount' inbound rule for some reason, continuing anyway..."}
+netsh.exe advfirewall firewall add rule name="Emotet_block" dir=out action=block localip=any remoteip="$textranges" description="$description" profile="$profiletype" interfacetype="$interfacetype"
+#if (-not $?) { "`nFailed to create '$rulename-#$icount' outbound rule for some reason, continuing anyway..."}
+
+
+
